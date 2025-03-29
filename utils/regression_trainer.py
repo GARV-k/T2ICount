ddb236862d32feacc0fa2b536e4e7ed7151a2068
cha15yq <55359961+cha15yq@users.noreply.github.com>
2025-03-29 23:28:29 +0000

    Update regression_trainer.py

--- utils/regression_trainer.py
+++ utils/regression_trainer.py
@@ -124,7 +124,7 @@ class Reg_Trainer(Trainer):
                 N = inputs.shape[0]
                 pred_den, sim_x2, sim_x1, fused_cross_attn = self.model(inputs, caption, gt_prompt_attn_mask)
                 fused_cross_attn_ = fused_cross_attn * gt_img_attn_mask
-                AN = fused_cross_attn_ >= 0.3 
+                AN = fused_cross_attn_ >= 0.5 
                 reg_loss = get_reg_loss(pred_den, gt_den_maps, threshold=1e-3 * 60)
                 P = gt_den_maps >= (1e-3 * 60)
                 rrc_loss_stage1 = RRC_loss(sim_x2, AN, P)
